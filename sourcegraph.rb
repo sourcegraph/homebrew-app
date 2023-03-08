af252479f4cd1cec89101d13f8a37df266d5f270
sourcegraph-buildkite <71296199+sourcegraph-buildkite@users.noreply.github.com>
2023-03-08 23:02:12 +0100

    Brew formula update for sourcegraph version 2023.03.23+204957.f499bb

--- sourcegraph.rb
+++ sourcegraph.rb
@@ -5,24 +5,24 @@
 class Sourcegraph < Formula
   desc "Code intelligence and search"
   homepage "https://github.com/sourcegraph/sourcegraph"
-  version "2023.03.23+204874.db2922"
+  version "2023.03.23+204957.f499bb"
   license "Sourcegraph Enterprise License (portions licensed under Apache 2)"
 
   depends_on "git"
   depends_on "sourcegraph/src-cli/src-cli"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/sourcegraph-app-releases/2023.03.23+204874.db2922/sourcegraph_2023.03.23+204874.db2922_darwin_amd64.zip"
-      sha256 "aeec33dc88fc7c9462ddc9cff4d1e7f2cdbecb02b7f04f069b6d9dd7947869f1"
+    if Hardware::CPU.arm?
+      url "https://storage.googleapis.com/sourcegraph-app-releases/2023.03.23+204957.f499bb/sourcegraph_2023.03.23+204957.f499bb_darwin_arm64.zip"
+      sha256 "def82a7e8ee542ca650f0689d69b3c5461f8fdcb5480192a57c993d93ddfb9a7"
 
       def install
         bin.install "sourcegraph"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/sourcegraph-app-releases/2023.03.23+204874.db2922/sourcegraph_2023.03.23+204874.db2922_darwin_arm64.zip"
-      sha256 "c0e95eb481b942a15a787176188a8405b5932546d672c931773e8e9b45c349f9"
+    if Hardware::CPU.intel?
+      url "https://storage.googleapis.com/sourcegraph-app-releases/2023.03.23+204957.f499bb/sourcegraph_2023.03.23+204957.f499bb_darwin_amd64.zip"
+      sha256 "14084d9c98d7ab5296a157052320291ca58005fc8953cd2335270c388ca85b83"
 
       def install
         bin.install "sourcegraph"
@@ -32,8 +32,8 @@ class Sourcegraph < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/sourcegraph-app-releases/2023.03.23+204874.db2922/sourcegraph_2023.03.23+204874.db2922_linux_amd64.zip"
-      sha256 "fd13ca1673650d0cce047f713bfa7c75542a4f74bc374ebbd6927d4688e4be58"
+      url "https://storage.googleapis.com/sourcegraph-app-releases/2023.03.23+204957.f499bb/sourcegraph_2023.03.23+204957.f499bb_linux_amd64.zip"
+      sha256 "a9fca597fc7a706970b5efa2d4a47708a3484bbc00f154f2afdc0aeef86d3f40"
 
       def install
         bin.install "sourcegraph"
